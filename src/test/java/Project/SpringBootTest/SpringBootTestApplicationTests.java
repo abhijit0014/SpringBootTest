d339e8bf6b45affb2c46e04f7b0474a8ad6dc061
Abhijit <abhijit0014@gmail.com>
2025-06-30 10:21:46 +0530

    initial commit

---
+++ src/test/java/Project/SpringBootTest/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
